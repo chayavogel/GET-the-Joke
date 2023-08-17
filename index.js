ab908ccd82025e17c76041685f5145780654fb1f
chaya <chayavogel1@gmail.com>
2023-08-17 10:43:42 -0400

    Create renderJokes function that renders the jokes from the API and the db.json onto the page. Add two event listeners to the punchline so it only reveales on hover. Changed the IDs of some elements in the html submission form bc I wanted to use them for the card.

--- index.js
+++ index.js
@@ -20,3 +20,31 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 })
+
+//Function to render jokes from API, db.json, and joke submission form
+
+function renderJokes(jokeObj) {
+    const jokeCardsContainer = document.getElementById("joke-cards-container")
+
+    const jokeCard = document.createElement("div")
+    jokeCard.setAttribute("id", "card")
+
+    const joke = document.createElement("p")
+    joke.innerText = jokeObj.setup
+    joke.setAttribute("id", "setup")
+
+    const punchline = document.createElement("p")
+    punchline.innerText = "Hover for punchline!"
+    punchline.setAttribute("id", "punchline")
+
+    jokeCardsContainer.append(jokeCard)
+    jokeCard.append(joke, punchline)
+
+    punchline.addEventListener("mouseover", function() {
+        punchline.innerText = jokeObj.punchline
+    })
+
+    punchline.addEventListener("mouseout", function() {
+        punchline.innerText = "Hover for punchline!"
+    })
+}
